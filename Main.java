738bdffc16a05c822b4c9a7bf481bce4fc102822
Akumatic <ugp@hotmail.de>
2018-10-19 16:21:38 +0200

    better input file handling

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+import java.io.File;
+
 class Main{
     public static void main(String[] args) {
-        GraphReader r = new GraphReader();
-        r.readData(args[0]);
+        if(args.length != 1){
+            System.err.println("Invalid amount of arguments - only one argument is allowed");
+            System.exit(1);
+        }
+        File f = new File(args[0]);
+        if(f.isFile()){
+            System.out.println("File found");
+            GraphReader r = new GraphReader(f);
+            r.readData(args[0]);
+        } else {
+            System.err.println("File does not exist or is a directory");
+            System.exit(1);
+        }
     }
 }
