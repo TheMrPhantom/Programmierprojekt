c90245e478b0ea451d6a3efe70583dd98d1092ac
TheMrPhantom <TheMrPhantom@users.noreply.github.com>
2018-12-04 16:47:59 +0100

    created min heap

--- ProPro/src/main/java/de/propro/backend/MinHeap.java
+++ ProPro/src/main/java/de/propro/backend/MinHeap.java
@@ -1,9 +1,26 @@
 package de.propro.backend;
 
+import org.jgrapht.util.*;
+
 public class MinHeap {
-	
+
+	FibonacciHeap<Integer> heap;
+
 	public MinHeap() {
-		
+		heap = new FibonacciHeap<Integer>();
+	}
+
+	public void push(int nodeIdx, int value) {
+		heap.insert(new FibonacciHeapNode<Integer>(value), nodeIdx);
+	}
+
+	public int pop() {
+		FibonacciHeapNode<Integer> value = heap.min();
+		heap.removeMin();
+		return value.getData();
 	}
 	
+	public void decreaseValue(int nodeIdx, int value) {
+		heap.decreaseKey(new FibonacciHeapNode<Integer>(value), nodeIdx);
+	}
 }
