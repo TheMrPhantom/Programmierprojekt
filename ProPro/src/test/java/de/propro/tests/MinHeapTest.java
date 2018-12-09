e0cb9ec6f7d9a82cb41df89af2bcf6306b1b287a
ayguenef <ayguenef@users.noreply.github.com>
2018-12-09 17:49:56 +0100

    Added tests for decreaseValue and pop

--- ProPro/src/test/java/de/propro/tests/MinHeapTest.java
+++ ProPro/src/test/java/de/propro/tests/MinHeapTest.java
@@ -98,5 +98,58 @@ public class MinHeapTest {
 		assertEquals(5, min.pop());
 
 	}
+	
+	@Test
+	public void testHeap5() {
+		CustomMinHeap min = new CustomMinHeap(3);
+		
+		min.decreaseValue(0, 5);
+		min.decreaseValue(1, 6);
+		min.decreaseValue(2, 7);
+
+		
+		assertEquals(0, min.pop());
+		assertEquals(1, min.pop());
+		assertEquals(2, min.pop());
+	
+	}
+	
+	@Test
+	public void testHeap6() {
+		CustomMinHeap min = new CustomMinHeap(15);
+		
+		min.decreaseValue(0, 10);
+		min.decreaseValue(1, 15);
+		min.decreaseValue(2, 20);
+		min.decreaseValue(3, 1);
+		min.decreaseValue(4, 42);
+		min.decreaseValue(5, 45);
+		min.decreaseValue(6, 50);
+		min.decreaseValue(7, 65);
+		min.decreaseValue(8, 80);
+		min.decreaseValue(9, 90);
+		min.decreaseValue(10, 500);
+		min.decreaseValue(11, 400);
+		min.decreaseValue(12, 800);
+		min.decreaseValue(13, 0);
+		min.decreaseValue(14, 300);
+		
+		assertEquals(13, min.pop());
+		assertEquals(3, min.pop());
+		assertEquals(0, min.pop());
+		assertEquals(1, min.pop());
+		assertEquals(2, min.pop());
+		assertEquals(4, min.pop());
+		assertEquals(5, min.pop());
+		assertEquals(6, min.pop());
+		assertEquals(7, min.pop());
+		assertEquals(8, min.pop());
+		assertEquals(9, min.pop());
+		assertEquals(14, min.pop());
+		assertEquals(11, min.pop());
+		assertEquals(10, min.pop());
+		assertEquals(12, min.pop());
+		
+	}
 
 }
