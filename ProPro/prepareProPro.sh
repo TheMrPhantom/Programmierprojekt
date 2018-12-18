38ab4306fc7a50b6053923666f50327916caf7f1
TheMrPhantom <TheMrPhantom@users.noreply.github.com>
2018-12-18 23:01:51 +0100

    Create prepareProPro.sh

---
+++ ProPro/prepareProPro.sh
@@ -0,0 +1,6 @@
+sudo apt-get install -y software-properties-common;
+sudo apt-get update;
+sudo apt-get install gradle;
+sudo add-apt-repository ppa:cwchien/gradle;
+sudo apt-get update;
+sudo apt upgrade gradle;
