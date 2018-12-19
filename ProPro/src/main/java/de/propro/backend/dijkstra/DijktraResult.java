a6de86a1a4dfe7df33c2fe155688799403bfd607
schieljn <st154910@stud.uni-stuttgart.de>
2018-12-19 17:02:40 +0100

    JavaDoc to DikstraResult.java

--- ProPro/src/main/java/de/propro/backend/dijkstra/DijktraResult.java
+++ ProPro/src/main/java/de/propro/backend/dijkstra/DijktraResult.java
@@ -2,6 +2,13 @@ package de.propro.backend.dijkstra;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * Container class for the result of the dijkstra algorithm
+ * 
+ * @author Justin
+ *
+ */
 public class DijktraResult {
 	public ArrayList<Integer> path;
 	public int length;
