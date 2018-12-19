a0f60041493a5ae541d13d996cba2a393c4443f7
schieljn <st154910@stud.uni-stuttgart.de>
2018-12-19 17:04:54 +0100

    JavaDoc to CustomReader.java

--- ProPro/src/main/java/de/propro/backend/reading/CustomReader.java
+++ ProPro/src/main/java/de/propro/backend/reading/CustomReader.java
@@ -12,11 +12,20 @@ public abstract class CustomReader implements Runnable {
 	protected final File file;
 	protected boolean isFinished;
 
+	/**
+	 * 
+	 * Initializes the attributes
+	 * 
+	 * @param file The graph file
+	 */
 	public CustomReader(File file) {
 		this.file = file;
 		this.isFinished = false;
 	}
 
+	/**
+	 * Abstract method for the reading thread
+	 */
 	@Override
 	public abstract void run();
 
