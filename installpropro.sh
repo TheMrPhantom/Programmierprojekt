fa7e0029d60b4fdf75b6cbcef5365599ae7f522d
themrphantom <diemilbe@gmx.net>
2019-02-13 18:46:16 +0100

    update

--- installpropro.sh
+++ installpropro.sh
@@ -5,6 +5,7 @@ sudo tar xf apache-tomcat-7.0.92.tar.gz -C /opt/tomcat
 sudo apt install default-jdk
 sudo apt install maven
 sudo $CATALINA_HOME/bin/startup.sh
+sudo rm $CATALINA_HOME/webapps/ROOT.war
 sudo rm $CATALINA_HOME/webapps/ROOT/ -r
 mvn -f ProProWeb/pom.xml clean install
 sudo mv ProProWeb/target/ProProWeb-0.1.war $CATALINA_HOME/webapps/ROOT.war
