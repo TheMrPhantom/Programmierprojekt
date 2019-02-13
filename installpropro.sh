265c33fe22656d27cf73f99ae8564ef502f26c8b
themrphantom <diemilbe@gmx.net>
2019-02-13 16:35:41 +0100

    new install file

---
+++ installpropro.sh
@@ -0,0 +1,4 @@
+wget http://www.trieuvan.com/apache/tomcat/tomcat-7/v7.0.90/bin/apache-tomcat-7.0.90.tar.gz
+sudo tar xf apache-tomcat-7.0.90.tar.gz -C /opt/tomcat
+sudo apt install default-jdk
+sudo apt install maven
