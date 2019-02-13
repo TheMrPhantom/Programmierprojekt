19bcd4ab0f33105c2080862f0bd8c770133ba4b8
themrphantom <diemilbe@gmx.net>
2019-02-13 16:43:01 +0100

    added mkdir

--- installpropro.sh
+++ installpropro.sh
@@ -1,4 +1,5 @@
 wget http://www.trieuvan.com/apache/tomcat/tomcat-7/v7.0.92/bin/apache-tomcat-7.0.92.tar.gz
+sudo mkdir /opt/tomcat
 sudo tar xf apache-tomcat-7.0.92.tar.gz -C /opt/tomcat
 sudo apt install default-jdk
 sudo apt install maven
