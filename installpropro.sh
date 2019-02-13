aabd9d1cd2fedee34113f8cab94ec4be01df30fc
themrphantom <diemilbe@gmx.net>
2019-02-13 17:11:05 +0100

    updated script

--- installpropro.sh
+++ installpropro.sh
@@ -1,3 +1,4 @@
+CATALINA_HOME=/opt/tomcat/apache-tomcat-7.0.92
 wget http://www.trieuvan.com/apache/tomcat/tomcat-7/v7.0.92/bin/apache-tomcat-7.0.92.tar.gz
 sudo mkdir /opt/tomcat
 sudo tar xf apache-tomcat-7.0.92.tar.gz -C /opt/tomcat
