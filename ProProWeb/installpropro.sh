ab4d31bb65de08e5c0e21417ad45f4ac44da16a6
TheMrPhantom <schieljn@studi.informatik.uni-stuttgart.de>
2019-10-23 17:38:06 +0200

    Moved install scripts to the right directory

---
+++ ProProWeb/installpropro.sh
@@ -0,0 +1,14 @@
+CATALINA_HOME=/opt/tomcat/apache-tomcat-7.0.96
+wget http://apache.mirror.digionline.de/tomcat/tomcat-7/v7.0.96/bin/apache-tomcat-7.0.96.tar.gz
+sudo mkdir /opt/tomcat
+sudo tar xf apache-tomcat-7.0.96.tar.gz -C /opt/tomcat
+sudo apt install default-jdk
+sudo apt install maven
+sudo $CATALINA_HOME/bin/startup.sh
+sudo rm $CATALINA_HOME/webapps/ROOT.war
+sudo rm $CATALINA_HOME/webapps/ROOT/ -r
+mvn clean install
+sudo mv target/ProProWeb-0.1.war $CATALINA_HOME/webapps/ROOT.war
+echo "Please insert the absolute path to the graph file:"
+read pathToFile
+curl "localhost:8080/api/internal/initGraph?path=$pathToFile"
