8f9995efb96b85c8e076d09ce0500d8c276b1b89
TheMrPhantom <36520162+TheMrPhantom@users.noreply.github.com>
2020-02-21 16:58:54 +0100

    Update installpropro.sh

--- installpropro.sh
+++ installpropro.sh
@@ -1,7 +1,7 @@
 CATALINA_HOME=/opt/tomcat/apache-tomcat-7.0.96
-wget http://apache.mirror.digionline.de/tomcat/tomcat-7/v7.0.96/bin/apache-tomcat-7.0.96.tar.gz
+wget http://apache.mirror.digionline.de/tomcat/tomcat-7/v7.0.100/bin/apache-tomcat-7.0.100.tar.gz
 sudo mkdir /opt/tomcat
-sudo tar xf apache-tomcat-7.0.96.tar.gz -C /opt/tomcat
+sudo tar xf apache-tomcat-7.0.100.tar.gz -C /opt/tomcat
 sudo apt install default-jdk
 sudo apt install maven
 sudo $CATALINA_HOME/bin/startup.sh
